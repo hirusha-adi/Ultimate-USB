78628a12dceb7b352abfa4f5ff5827a789fd0fd9
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-02-27 08:40:36 +0530

    wont work due to import path issues

--- src/modules/wifi.py
+++ src/modules/wifi.py
@@ -59,18 +59,3 @@ class Wifi:
         self.processOutput()
         if self.file:
             self.getOutputFile()
-
-
-if __name__ == "__main__":
-    import time
-    if os.name == 'nt':
-        with open("wifi-output.txt", "w+", encoding="utf-8") as file_wifi_passwords, open("errors.txt", "w+", encoding="utf-8") as file_wifi_passwords_errors:
-            file_wifi_passwords.write(
-                "{seperator}\nWifi Passwords\n{seperator}".format(seperator='*'*20))
-            file_wifi_passwords_errors.write(
-                "{seperator}\nWifi Passwords\n{seperator}".format(seperator='*'*20))
-            obj = Wifi(file=file_wifi_passwords,
-                       error_file=file_wifi_passwords_errors)
-            obj.run()
-            print(obj.getOutput())
-        time.sleep(10)
